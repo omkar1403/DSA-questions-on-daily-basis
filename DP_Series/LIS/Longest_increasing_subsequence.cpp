33ee99f6047ccaffda82972b63b0704b22eb1ec8
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-05-09 22:35:43 +0530

    Longest Increasing

--- DP_Series/LIS/Longest_increasing_subsequence.cpp
+++ DP_Series/LIS/Longest_increasing_subsequence.cpp
@@ -30,3 +30,23 @@ public:
      return maxi(nums,0,-1,n,dp);
     }
 };
+
+
+
+// 2nd Tabulation approach 
+
+ int lengthOfLIS(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>dp(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(arr[prev]<arr[i]){
+                    dp[i]=max(dp[i],1+dp[prev]);
+                }
+            }
+            maxi=max(dp[i],maxi);
+        } 
+        return maxi;   
+    }
+
