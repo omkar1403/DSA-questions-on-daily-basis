e9ca86c3ddc016b874e97a83137c5c38ccc76c4e
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-05-27 23:53:03 +0530

    max depth

---
+++ TREES/maximum_depth_tree.cpp
@@ -0,0 +1,35 @@
+/*maine kuch nahi kiya sirf level order traversal nikala jisme maine 2d array me push karna start kar diya and 
+last me uske jitne bhi rows hai vo return kar diye accordingly means output.size()*/
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+       vector<vector<int>>output;
+       queue<TreeNode*>q;
+       if(root==NULL){
+        return 0;
+       }
+       q.push(root);
+       while(!q.empty()){
+       int k=q.size();
+       vector<int>ans;
+       for(int i=0;i<k;i++){
+        TreeNode* node=q.front();
+
+         ans.push_back(node->val);
+           q.pop();
+           if(node->left !=NULL){
+        q.push(node->left);
+       }
+       if(node->right !=NULL){
+        q.push(node->right);
+       }
+          
+       }
+       
+       output.push_back(ans);
+
+       }
+       return output.size();
+
+    }
+};
