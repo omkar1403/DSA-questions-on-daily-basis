5ff4c6229c14032a8a5d7b073fbb0ab2fcf0bd4e
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-06-08 12:15:17 +0530

    Trees lowest common

--- TREES/lowest_common_ancestor.cpp
+++ TREES/lowest_common_ancestor.cpp
@@ -50,3 +50,23 @@ public:
 
     }
 };
+
+
+//approach2 if we want to to backtrack to the root then we have to do this way
+
+ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+       if(root==NULL || root==p || root==q){
+        return root;
+       }
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+
+        if(left==NULL){
+            return right;
+        }
+         if(right==NULL){
+            return left;
+        }
+        return root;
+
+    }
