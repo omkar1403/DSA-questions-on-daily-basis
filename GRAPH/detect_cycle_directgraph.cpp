17a89847b4c51ea3b5ad9c2f581970267c85ad00
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-06-28 15:27:04 +0530

    cycle in directed

---
+++ GRAPH/detect_cycle_directgraph.cpp
@@ -0,0 +1,34 @@
+/*here we have to make the pathvis[node]=0 again soo that it will predict whether it is making cycle or not*/
+
+
+bool dfs(int node, vector<int>adj[], int vis[], int pathvis[]) {
+       vis[node]=1;
+       pathvis[node]=1;
+       for(auto i:adj[node]){
+           if(!vis[i]){
+             if(dfs(i,adj,vis,pathvis)==true){ //if it is true and not visited then return true
+                 return true;
+             }  
+           }else if(pathvis[i]){  //if it is also true then this initial point of cycle soo it is true
+               return true;
+           }
+       }
+       pathvis[node]=0; //make it 0 when return from the cycle
+       return false;
+    }
+    
+    
+    
+    bool isCyclic(int V, vector<int> adj[]) {
+        int vis[V] = {0}; 
+        int pathvis[V]={0};
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(dfs(i,adj,vis,pathvis)==true){
+                    return true;
+                }
+            }
+        }
+        
+       return false; 
+    }
