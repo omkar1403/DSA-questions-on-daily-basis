7b02c50b8be8df10e98187abffab8f4f041eef7b
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-07-01 00:12:58 +0530

    shortest path

--- DP_Series/LIS/Longest_increasing_subsequence.cpp
+++ DP_Series/LIS/Longest_increasing_subsequence.cpp
@@ -50,3 +50,23 @@ public:
         return maxi;   
     }
 
+
+
+//approach 3
+int longestSubsequence(int n, int a[])
+    {
+      if(n==0){
+          return 0;
+      }
+      vector<int>ans;
+      ans.push_back(a[0]);
+      for(int i=1;i<n;i++){
+          if(a[i]>ans.back()){
+              ans.push_back(a[i]);
+          }else{
+              int ind=lower_bound(ans.begin(),ans.end(),a[i])-ans.begin();
+              ans[ind]=a[i];
+          }
+      }
+      return ans.size();
+    }
